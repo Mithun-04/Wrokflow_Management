985fdc3db11fb70b729347e5e9eb1e4a0c8e1841
Surya <www.cosmicpegasus74@gmail.com>
2025-03-17 16:26:13 +0530

    done

--- demo/src/Components/popup.jsx
+++ demo/src/Components/popup.jsx
@@ -3,14 +3,17 @@ import "../styles/popup.css";
 
 function Popup({ setModal }) {
   return (
-    <div className="popup-container">
-      <button className="popup-close-btn" onClick={() => setModal(false)}>✖</button>
-      <h1 className="popup-title">Create Project</h1>
-      <form className="popup-form">
-        <label className="popup-label" htmlFor="project-name">Project Name:</label>
-        <input className="popup-input" type="text" id="project-name" name="project-name" required />
-        <button className="popup-submit-btn" type="submit" onClick={() => setModal(false)}>Submit</button>
-      </form>
+    <div className="Dummy">
+      <div className="overlay"></div>
+      <div className="popup-container">
+        <button className="popup-close-btn" onClick={() => setModal(false)}>✖</button>
+        <h1 className="popup-title">Create Project</h1>
+        <form className="popup-form">
+          <label className="popup-label" htmlFor="project-name">Project Name:</label>
+          <input className="popup-input" type="text" id="project-name" name="project-name" required />
+          <button className="popup-submit-btn" type="submit" onClick={() => setModal(false)}>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
